8d1f65930fb867c5d5576788c90b8f496910e3cb
Claudia Teng <sydeng102@gmail.com>
2023-07-12 11:22:39 +0800

    feat: remove demo parts

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -3,18 +3,16 @@
 import styles from './ProductItem.module.sass'
 import StarIcon from '../../assets/star.png'
 
-function Product({ product }) {
+function Product({}) {
   return (
     <>
       <div className={styles.card}>
-        <img alt='product' src={product.image} />
-        <p>{product.title}</p>
-        <p className={styles.price}>${product.price}</p>
+        <img alt='product' src='https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg' />
+        <p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
+        <p className={styles.price}>$109.95</p>
         <div className={styles.rating}>
           <img alt='rating' src={StarIcon} />
-          <span>
-            {product.rating.rate} {`(${product.rating.count})`}
-          </span>
+          <span>3.9 {`(120)`}</span>
         </div>
       </div>
     </>
--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,26 +1,12 @@
 import { useState, useEffect } from 'react'
-// import { useNavigate } from 'react-router-dom'
 import styles from './Product.module.sass'
 import ProductItem from '../../components/ProductItem/ProductItem'
 import { productApi } from '../../api/productApi'
 
 function Product() {
-  const [productList, setProductList] = useState([])
-
-  useEffect(() => {
-    async function getProductList() {
-      const response = await productApi.getProductList()
-      setProductList(response)
-    }
-
-    getProductList()
-  }, [])
-
   return (
     <div className={styles.container}>
-      {productList.map((product) => {
-        return <ProductItem product={product} key={product.id} />
-      })}
+      <ProductItem />
     </div>
   )
 }
